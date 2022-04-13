4288ce93f393ceaf6eb156034efbbfec4771e8ec
Quiquiriquin <enrique.gintelligence@gmail.com>
2022-04-13 00:08:33 -0500

    Feat: Finished task

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const dashboardRouter = require('./routes/dashboard');
